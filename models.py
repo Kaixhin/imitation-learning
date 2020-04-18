99533968f8f4f3d4316a8668ffa4005799223a4d
Kaixhin <design@kaixhin.com>
2020-04-18 10:35:18 +0100

    Replace AddLinearEmbed with one_hot and Lienear

--- models.py
+++ models.py
@@ -1,6 +1,12 @@
 import torch
 from torch import nn
 from torch.distributions import Categorical
+from torch.nn import functional as F
+
+
+# Concatenates the state and one-hot version of an action
+def _join_state_action(state, action, action_size):
+  return torch.cat([state, F.one_hot(action, action_size).to(dtype=torch.float32)], dim=1)
 
 
 class Actor(nn.Module):
@@ -38,25 +44,15 @@ class ActorCritic(nn.Module):
     return self.actor(state).log_prob(action)
 
 
-class AddLinearEmbed(nn.Module):
-  def __init__(self, linear_size, embedding_size, output_size):
-    super().__init__()
-    self.linear = nn.Linear(linear_size, output_size)
-    self.embedding = nn.Embedding(embedding_size, output_size)
-
-  def forward(self, x):
-    return self.linear(x[0]) + self.embedding(x[1])
-
-
 class GAILDiscriminator(nn.Module):
   def __init__(self, state_size, action_size, hidden_size, state_only=True):
     super().__init__()
-    self.state_only = state_only
-    input_layer = nn.Linear(state_size, hidden_size) if state_only else AddLinearEmbed(state_size, action_size, hidden_size)
+    self.action_size, self.state_only = action_size, state_only
+    input_layer = nn.Linear(state_size if state_only else state_size + action_size, hidden_size)
     self.discriminator = nn.Sequential(input_layer, nn.Tanh(), nn.Linear(hidden_size, hidden_size), nn.Tanh(), nn.Linear(hidden_size, 1), nn.Sigmoid())
 
   def forward(self, state, action):
-    D = self.discriminator(state if self.state_only else (state, action)).squeeze(dim=1)
+    D = self.discriminator(state if self.state_only else _join_state_action(state, action, self.action_size)).squeeze(dim=1)
     return D
   
   def predict_reward(self, state, action):
@@ -67,19 +63,19 @@ class GAILDiscriminator(nn.Module):
 class AIRLDiscriminator(nn.Module):
   def __init__(self, state_size, action_size, hidden_size, discount, state_only=False):
     super().__init__()
-    self.state_only = state_only
+    self.action_size, self.state_only = action_size, state_only
     self.discount = discount
-    self.g = nn.Linear(state_size, 1) if state_only else AddLinearEmbed(state_size, action_size, 1)  # Reward function r
+    self.g = nn.Linear(state_size if state_only else state_size + action_size, 1)  # Reward function r
     self.h = nn.Sequential(nn.Linear(state_size, hidden_size), nn.Tanh(), nn.Linear(hidden_size, hidden_size), nn.Tanh(), nn.Linear(hidden_size, 1))  # Shaping function Φ
 
-  def reward(self, state_action):
-    return self.g(state_action[0] if self.state_only else state_action).squeeze(dim=1)
+  def reward(self, state, action):
+    return self.g(state if self.state_only else _join_state_action(state, action, self.action_size)).squeeze(dim=1)
 
   def value(self, state):
     return self.h(state).squeeze(dim=1)
 
   def forward(self, state, action, next_state, policy):
-    f = self.reward((state, ) if self.state_only else (state, action)) + self.discount * self.value(next_state) - self.value(state)
+    f = self.reward(state, action) + self.discount * self.value(next_state) - self.value(state)
     f_exp = f.exp()
     return f_exp / (f_exp + policy)
 
