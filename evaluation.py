67ff003da0ca4388ea3fa35facda4c632632e537
Dan Lillrank <dan.lillrank@gmail.com>
2021-04-27 19:17:50 +0900

    fixed some error in code and changed evaluation input format

--- evaluation.py
+++ evaluation.py
@@ -1,11 +1,11 @@
 import torch
 
-from environments import PendulumEnv
+from environments import PendulumEnv, D4RLEnv
 
 
 # Evaluate agent with deterministic policy π
-def evaluate_agent(agent, episodes, return_trajectories=False, Env=PendulumEnv, seed=1, render=False):
-  env = Env
+def evaluate_agent(agent, episodes, return_trajectories=False, Env=PendulumEnv, env_name="", seed=1, render=False):
+  env = Env(env_name)
   env.seed(seed)
 
   returns, trajectories = [], []
@@ -19,7 +19,6 @@ def evaluate_agent(agent, episodes, return_trajectories=False, Env=PendulumEnv, 
       with torch.no_grad():
         action = agent.greedy_action(state)  # take action greedily
         state, reward, terminal = env.step(action)
-
         if return_trajectories:
           states.append(state)
           actions.append(action)
@@ -29,5 +28,5 @@ def evaluate_agent(agent, episodes, return_trajectories=False, Env=PendulumEnv, 
       # Collect trajectory data (including terminal signal, which may be needed for offline learning)
       terminals = torch.cat([torch.zeros(len(rewards) - 1), torch.ones(1)])
       trajectories.append(dict(states=torch.cat(states), actions=torch.cat(actions), rewards=torch.tensor(rewards, dtype=torch.float32), terminals=terminals))
-
+  env.close()
   return (returns, trajectories) if return_trajectories else returns
