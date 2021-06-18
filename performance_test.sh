6009310659f3ecb0d5bdea27a4b42ad3550afa28
Harimus <dan.lillrank@gmail.com>
2021-06-18 19:12:55 +0900

    updated performance test

--- performance_test.sh
+++ performance_test.sh
@@ -5,10 +5,12 @@ algos=("BC" "AIRL" "DRIL" "FAIRL" "GAIL" "GMMIL" "RED")
 
 for algo in ${algos[@]}; do
     if [[ $# -eq 1 ]]; then
-      echo "running python3 main.py algorithm=$algo/$env check_memory_usage=true hyperparam_opt=empty hydra/sweeper=basic"
-      python3 main.py algorithm=$algo/$env check_memory_usage=true hyperparam_opt=empty hydra/sweeper=basic
+      #echo "running python3 main.py algorithm=$algo/$env check_memory_usage=true hyperparam_opt=empty hydra/sweeper=basic"
+      #python3 main.py algorithm=$algo/$env check_memory_usage=true hyperparam_opt=empty hydra/sweeper=basic
+      echo "running mprof run main.py algorithm=$algo/$env  hyperparam_opt=empty hydra/sweeper=basic "
+      mprof run main.py algorithm=$algo/$env  hyperparam_opt=empty hydra/sweeper=basic
     else
-      echo "running python3 main.py algorithm=$algo/$env check_time_usage=true hyperparam_opt=empty hydra/sweeper=basic"
-      python3 main.py algorithm=$algo/$env check_time_usage=true hyperparam_opt=empty hydra/sweeper=basic
+      echo "running python3 main.py algorithm=$algo/$env steps=1000000 check_time_usage=true hyperparam_opt=empty hydra/sweeper=basic"
+      python3 main.py algorithm=$algo/$env steps=1000000 check_time_usage=true hyperparam_opt=empty hydra/sweeper=basic
     fi
 done
