f2a48799730fef1827995e0a6c4892c5af6715dc
Harimus <dan.lillrank@gmail.com>
2021-06-21 16:15:41 +0900

    added wonky performance test for mprof. need to make the script terminate on ctrl c properly (TODO)

--- performance_test.sh
+++ performance_test.sh
@@ -2,15 +2,18 @@
 
 env='hopper'
 algos=("BC" "AIRL" "DRIL" "FAIRL" "GAIL" "GMMIL" "RED")
+count=5
 
 for algo in ${algos[@]}; do
     if [[ $# -eq 1 ]]; then
-      #echo "running python3 main.py algorithm=$algo/$env check_memory_usage=true hyperparam_opt=empty hydra/sweeper=basic"
-      #python3 main.py algorithm=$algo/$env check_memory_usage=true hyperparam_opt=empty hydra/sweeper=basic
-      echo "running mprof run main.py algorithm=$algo/$env  hyperparam_opt=empty hydra/sweeper=basic "
-      mprof run main.py algorithm=$algo/$env  hyperparam_opt=empty hydra/sweeper=basic
+    for i in $(seq $count); do
+        rm ./mprofile_*.dat # clean mprof  
+        echo "running mprof run main.py algorithm=$algo/$env  hyperparam_opt=empty hydra/sweeper=basic "
+        mprof run main.py algorithm=$algo/$env  hyperparam_opt=empty hydra/sweeper=basic
+        awk 'BEGIN{a=0}{if ($2 > 0+a) a=$2} END{print a}' mprofile_*.dat >> "./${algo}_${env}_time.txt"
+    done
     else
       echo "running python3 main.py algorithm=$algo/$env steps=1000000 check_time_usage=true hyperparam_opt=empty hydra/sweeper=basic"
-      python3 main.py algorithm=$algo/$env steps=1000000 check_time_usage=true hyperparam_opt=empty hydra/sweeper=basic
+      python3 main.py -m algorithm=$algo/$env steps=1000000 check_time_usage=true seed=1,2,3,4,5 hyperparam_opt=empty hydra/sweeper=basic
     fi
 done
