805ca177095b9de14a8298ef7dc7b8858df4b128
Harimus <dan.lillrank@gmail.com>
2021-10-26 23:58:15 +0900

    added SAC and SQIL as option in scripts/run_seed_experiments.sh and also added option to specify number of seeds to run in list.

--- scripts/run_seed_experiments.sh
+++ scripts/run_seed_experiments.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 envs=('ant' 'halfcheetah' 'hopper' 'walker2d')
-algos=("BC" "AIRL" "DRIL" "FAIRL" "GAIL" "GMMIL" "PPO" "PUGAIL" "RED")
+algos=("BC" "AIRL" "DRIL" "FAIRL" "GAIL" "GMMIL" "PPO" "PUGAIL" "RED", "SAC", "SQIL")
 [[ " ${algos[@]} " =~ " $1 " ]] && echo "algorithm=$1" || { echo "invalid input, first input must be one of ${algos[*]} "; exit 1; }
+
+if [[ $# -eq 3 ]]; then
+[[ " ${envs[@]} " =~ " $2 " ]] && echo "environment=$2" || { echo "invalid input, second input must be empty or one of ${envs[*]} "; exit 1; }
+
+    echo "running python3 main.py -m algorithm=$1/$2 hyperparam_opt=empty seed=$3 hydra/sweeper=basic"
+    python3 main.py -m algorithm=$1/$2 hyperparam_opt=empty seed=$3 hydra/sweeper=basic
+exit 1;
+fi
 
 if [[ $# -eq 2 ]]; then
 [[ " ${envs[@]} " =~ " $2 " ]] && echo "environment=$2" || { echo "invalid input, second input must be empty or one of ${envs[*]} "; exit 1; }
