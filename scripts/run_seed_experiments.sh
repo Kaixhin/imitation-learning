2260749e2a62ed62f4b9dc7cf9f10ff9956e8c45
Harimus <dan.lillrank@gmail.com>
2021-10-27 00:03:30 +0900

    fixed error and now also allows to add hydra param directly if wanted

--- scripts/run_seed_experiments.sh
+++ scripts/run_seed_experiments.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 envs=('ant' 'halfcheetah' 'hopper' 'walker2d')
-algos=("BC" "AIRL" "DRIL" "FAIRL" "GAIL" "GMMIL" "PPO" "PUGAIL" "RED", "SAC", "SQIL")
+algos=("BC" "AIRL" "DRIL" "FAIRL" "GAIL" "GMMIL" "PPO" "PUGAIL" "RED", "SAC" "SQIL")
 [[ " ${algos[@]} " =~ " $1 " ]] && echo "algorithm=$1" || { echo "invalid input, first input must be one of ${algos[*]} "; exit 1; }
 
+if [[ $# -eq 4 ]]; then
+[[ " ${envs[@]} " =~ " $2 " ]] && echo "environment=$2" || { echo "invalid input, second input must be empty or one of ${envs[*]} "; exit 1; }
+
+    echo "running python3 main.py -m algorithm=$1/$2 hyperparam_opt=empty seed=$3 hydra/sweeper=basic $4"
+    python3 main.py -m algorithm=$1/$2 hyperparam_opt=empty seed=$3 hydra/sweeper=basic $4
+exit 1;
+fi
 if [[ $# -eq 3 ]]; then
 [[ " ${envs[@]} " =~ " $2 " ]] && echo "environment=$2" || { echo "invalid input, second input must be empty or one of ${envs[*]} "; exit 1; }
 
