e69d473009e231726c720da12dc13b844cbfb45d
Dan Ogawa Lillrank <dan.lillran@gmail.com>
2021-11-08 17:28:34 +0000

    added script for running parallel seed runs easily

---
+++ scripts/run_parallell_seed_experiments.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+NUM_THREADS=1
+SEEDS_PER_PROCESS=2
+TOTAL_SEEDS=10
+LOOP=($(seq 0 $SEEDS_PER_PROCESS $((TOTAL_SEEDS - SEEDS_PER_PROCESS))))
+envs=('ant' 'halfcheetah' 'hopper' 'walker2d')
+algos=("BC"  "DRIL" "GAIL" "GMMIL"  "RED", "SAC" "SQIL")
+[[ " ${algos[@]} " =~ " $1 " ]] && echo "algorithm=$1" || { echo "invalid input, first input must be one of ${algos[*]} "; exit 1; }
+
+for env in ${envs[@]}; do
+  #echo "running python3 main.py -m algorithm=$1/$env hyperparam_opt=empty seed=1,2,3,4,5 hydra/sweeper=basic"
+  for seed_num in ${LOOP[@]}; do
+  seed_end=$((seed_num + SEEDS_PER_PROCESS - 1))
+  seeds="$(seq -s ',' $seed_num 1 $seed_end)"
+  #OMP_NUM_THREADS=$NUM_THREADS python3 main.py -m algorithm=$1/$env hyperparam_opt=empty seed=$seeds hydra/sweeper=basic hydra.sweep.dir=./outputs/par_seed_sweeper_$1_$env/$seed_num &
+  echo "OMP_NUM_THREADS=$NUM_THREADS python3 main.py -m algorithm=$1/$env hyperparam_opt=empty seed=$seeds hydra/sweeper=basic hydra.sweep.dir=./outputs/par_seed_sweeper_$1_$env/$seed_num &"
+  done
+done
+wait 
