Fix RED bandwidth calculation

models.py
@@ -231,7 +231,7 @@ class REDDiscriminator(nn.Module):
   # Originally, sets σ based such that r(s, a) from expert demonstrations ≈ 1; instead this uses kernel median heuristic (same as GMMIL)
   def set_sigma(self, expert_state, expert_action):
     prediction, target = self.forward(expert_state, expert_action)
-    self.sigma_1 = 1 / _squared_distance(prediction.transpose(0, 1), target.transpose(0, 1)).median().item()
+    self.sigma_1 = 1 / _squared_distance(prediction, target).median().item()
 
   def predict_reward(self, state, action):
     prediction, target = self.forward(state, action)