17d2961f12cee26afe66c209682098ea95f07c3a
Kaixhin <design@kaixhin.com>
2023-03-08 14:37:22 +0900

    Clean imports in all.py

--- all.py
+++ all.py
@@ -4,19 +4,16 @@ from pathlib import Path
 import random
 
 import hydra
-from hydra.utils import get_original_cwd
-import torch.multiprocessing as mp 
 import numpy as np
 from omegaconf import DictConfig
-import torch
-from torch import optim
-from torch.utils.data import DataLoader
-from tqdm import tqdm
+# import torch
+from torch import multiprocessing as mp
 
 from main import run 
 from environments import get_all_env_baseline
 
-#torch.set_num_threads(1) This work the best, but probably slower
+
+# torch.set_num_threads(1) This work the best, but probably slower
 if __name__ == '__main__': mp.set_start_method('forkserver') #This circumvent deadlock
 
 
@@ -29,7 +26,7 @@ def pool_wrapper(cfg):
 @hydra.main(version_base=None, config_path='conf', config_name='all_config')
 def all(cfg: DictConfig):
   all_envs = dict(ant='ant-expert-v2', halfcheetah='halfcheetah-expert-v2', hopper='hopper-expert-v2', walker2d='walker2d-expert-v2')
-  filename = os.path.join(get_original_cwd(), 'normalization_data.npz')
+  filename = os.path.join(hydra.utils.get_original_cwd(), 'normalization_data.npz')
   all_env_cfgs = [] 
   seed = random.randint(0, 99)
   Path(f"./seed{seed}.txt").touch()
