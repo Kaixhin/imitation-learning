1226c4147953618095ac969791c794b02fea398e
Kaixhin <design@kaixhin.com>
2023-03-10 23:19:45 +0900

    Fix sweep plot ordering + adapt for BC

--- scripts/print_plot_sweep_results.py
+++ scripts/print_plot_sweep_results.py
@@ -8,8 +8,8 @@ import pandas as pd
 import torch
 import yaml
 
-ENVS = ['ant', 'halfcheetah', 'hopper', 'walker2d']  # TODO: Relative import from environments?
 
+ENVS = ['ant', 'halfcheetah', 'hopper', 'walker2d']  # TODO: Relative import from environments?
 
 
 parser = argparse.ArgumentParser(description='Plot hyperparameter sweep results')
@@ -39,28 +39,28 @@ for entry in sorted(entries, key=lambda e: int(e.name)):  # Natural sorting (so 
   experiments['min_avg_test_return'].append(experiments['test_returns'][-1].mean(axis=(1, 2)).min())  # Take mean for each env, then take min over envs
 
 
-# Create dataframe and sort
-df = pd.DataFrame(experiments).sort_values('min_avg_test_return', ascending=False)
+# Create dataframe
+df = pd.DataFrame(experiments)
 # Remove fixed hyperparameters
 nunique = df[[c for c in df.columns if c not in ['test_steps', 'test_returns', 'min_avg_test_return']]].nunique()
 df = df.drop(nunique[nunique == 1].index, axis=1)
 # Infer best datatypes for data
 df = df.apply(pd.to_numeric, errors='ignore')  # Converts to numeric data if possible, otherwise keeps original
 df = df.convert_dtypes()  # Converts categorical data to string (np.arrays left as object)
-# Print dataframe
-print(df)
+# Print sorted dataframe
+print(df.sort_values('min_avg_test_return', ascending=False))
 
 
 # Plot returns from all experiments
-fig, cmap = make_subplots(rows=5, cols=6, shared_xaxes=True, shared_yaxes=True, subplot_titles=list(map(lambda n: f'{n:.3f}', df['min_avg_test_return']))), px.colors.qualitative.Plotly  # 30 experiments
+fig, cmap = make_subplots(rows=5, cols=6, shared_xaxes=True, shared_yaxes=True, subplot_titles=list(map(lambda i: f'#{i[0]}: Min Ret. {i[1]:.3f}', enumerate(df['min_avg_test_return'])))), px.colors.qualitative.Plotly  # 30 experiments
 for idx, row in df.iterrows():
   for e, env in enumerate(ENVS):
-    fig.add_trace(go.Scatter(x=row['test_steps'], y=row['test_returns'][e].mean(axis=1), mode='lines', line={'color': cmap[e]}, name=env, showlegend=idx == 0), row=(idx // 6) + 1, col=(idx % 6) + 1)
+    fig.add_trace(go.Scatter(x=row['test_steps'], y=row['test_returns'][e].mean(axis=1), mode='markers' if row['test_returns'].shape[1] == 1 else 'lines', line={'color': cmap[e]}, name=env, showlegend=idx == 0), row=(idx // 6) + 1, col=(idx % 6) + 1)
 fig.update_annotations(font_size=13)  # Reduce title font size
-fig.update_yaxes(range=(-0.1, 1.1))  # Set shared plot ranges
+fig.update_yaxes(range=(-0.1, 1.5))  # Set shared plot ranges
 fig.show()
 
-# Show parallel coordinates plot
+# Plot parallel coordinates for hyperparameter search
 fig = go.Figure()
 dimensions = []
 for c in df.columns:
